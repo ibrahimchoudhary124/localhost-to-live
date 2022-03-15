7df6a14ea5d1d195afaa1790ff80c3b727563ea5
ibrahimchoudhary124 <101384415+ibrahimchoudhary124@users.noreply.github.com>
2022-03-15 17:35:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var url = require("url");
 var bodyParser = require('body-parser');
 
-app.use(bodyParser());
+app.use(bodyParser.OptionsText());
 
 var clientResponseRef;
 app.get('/*', (req, res) => {
